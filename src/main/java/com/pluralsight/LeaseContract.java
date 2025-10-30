50763c48785061890d1edc32623984fd61a0dc5e
nisaking03 <nisa.king03@gmail.com>
2025-10-30 15:56:05 -0400

    adding comments

--- src/main/java/com/pluralsight/LeaseContract.java
+++ src/main/java/com/pluralsight/LeaseContract.java
@@ -16,3 +16,11 @@ public class LeaseContract extends Contract{
     }
 
 }
+//• Expected Ending Value  (50% of the original price)
+//• Lease Fee (7% of the original price)
+//• Monthly payment based on
+//• All leases are financed at 4.0% for 36 months
+//Methods will include a constructor and getters and setters for all fields except
+//total price and monthly payment.  You should provide overrides for
+//getTotalPrice() and getMonthlyPayment() that will return computed
+//values based on the rules above.
