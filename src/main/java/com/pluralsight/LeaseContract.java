0244abbc833f3bdf014b00349f79ef5a0a199abc
nisaking03 <nisa.king03@gmail.com>
2025-11-05 14:11:58 -0500

    Need to finish UI

--- src/main/java/com/pluralsight/LeaseContract.java
+++ src/main/java/com/pluralsight/LeaseContract.java
@@ -1,21 +1,25 @@
 package com.pluralsight;
 
 public class LeaseContract extends Contract{
-    private double expectedEndingValue;
+    private double expectedValue;
     private double leaseFee;
 
     public LeaseContract(Vehicle vehicleSold, String customerEmail, String customerName,
-                         String date, double expectedEndingValue, double leaseFee) {
+                         String date, double expectedValue, double leaseFee) {
         super(vehicleSold, customerEmail, customerName, date);
 
         // Calculate the lease-specific values:
-        this.expectedEndingValue = vehicleSold.getPrice() * 0.5; // 50% of the original price
-        this.leaseFee = vehicleSold.getPrice() * 0.07;           // 7% lease fee
+        this.expectedValue = vehicleSold.getPrice() * 0.5; // Expected Ending Value  (50% of the original price)
+        this.leaseFee = vehicleSold.getPrice() * 0.07;     // Lease Fee (7% of the original price)
 
     }
 
+    // Methods will include a constructor and getters and setters for all fields except
+    // total price and monthly payment.  You should provide overrides for
+    // getTotalPrice() and getMonthlyPayment() that will return computed
+
     public double getExpectedEndingValue() {
-        return expectedEndingValue;
+        return expectedValue;
     }
 
     public double getLeaseFee() {
@@ -30,9 +34,8 @@ public class LeaseContract extends Contract{
 
     @Override
     public double getMonthlyPayment() {
-        //Calculate based on 4.0% interest, 36 months
-        //Use the formula: (adjusted capitalized cost - residual value) /
-        //term + monthly interest charge
+        // Monthly payment based on
+        // All leases are financed at 4.0% for 36 months
         double totalPrice = getTotalPrice();
 
         double annualRate = 0.04;
@@ -44,11 +47,3 @@ public class LeaseContract extends Contract{
     }
 
 }
-//• Expected Ending Value  (50% of the original price)
-//• Lease Fee (7% of the original price)
-//• Monthly payment based on
-//• All leases are financed at 4.0% for 36 months
-//Methods will include a constructor and getters and setters for all fields except
-//total price and monthly payment.  You should provide overrides for
-//getTotalPrice() and getMonthlyPayment() that will return computed
-//values based on the rules above.
